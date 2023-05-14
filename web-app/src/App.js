7844109f31d2273c6fc5076f2113f25b77fef25c
Saad5400 <sdbtwa@gmail.com>
2023-05-14 14:59:41 +0300

    added rtl and base navbar

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import Layout from './components/shared/Layout';
+
+function Sample() {
+  return (
+    <div className="hero min-h-screen bg-base-200">
+      <div className="text-center hero-content">
+        <div className="max-w-md">
+          <h1 className="mb-5 text-5xl font-bold">
+            رياكت بالعربي
+          </h1>
+          <p className="mb-5">
+            تعلم رياكت باللغة العربية
+          </p>
+          <button className="btn btn-primary">تعلم الآن</button>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div dir='rtl'>
+      <Layout>
+        <Sample />
+      </Layout>
     </div>
   );
 }
---
+++ web-app/src/components/shared/Layout.js
@@ -0,0 +1,57 @@
+import * as Icons from "./Icons";
+
+const drawerId = "main-drawer";
+
+function Navbar() {
+    return (
+        <div>
+            <div className="navbar bg-base-100">
+                <div className="flex-none">
+                    <label className="btn btn-square btn-ghost" htmlFor={drawerId}>
+                        <Icons.Menu />
+                    </label>
+                </div>
+                <div className="flex-1">
+                    <a className="btn btn-ghost normal-case text-xl">رياكت بالعربي</a>
+                </div>
+                <div className="flex-none">
+                    <button className="btn btn-square btn-ghost">
+                        <Icons.Elipses />
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+
+function Drawer(props) {
+    return (
+        <div>
+            <div className="drawer">
+                <input id={drawerId} type="checkbox" className="drawer-toggle" />
+                <div className="drawer-content flex flex-col">
+                    {props.children}
+                </div>
+                <div className="drawer-side">
+                    <label htmlFor={drawerId} className="drawer-overlay"></label>
+                    <ul className="menu p-4 w-80 bg-base-100">
+                        <li><a>Sidebar Item 1</a></li>
+                        <li><a>Sidebar Item 2</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default function Layout(props) {
+    return (
+        <div>
+            <Navbar />
+            <Drawer>
+                {props.children}
+            </Drawer>
+        </div>
+    );
+}
