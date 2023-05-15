c0d32e618e56e52a1b07a0d6877f3f7fc4fb5b86
Saad5400 <sdbtwa@gmail.com>
2023-05-15 04:09:33 +0300

    added homepage with some cool effects

--- web-app/src/App.jsx
+++ web-app/src/App.jsx
@@ -1,51 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Layout from './components/shared/Layout';
-
-function Sample() {
-  return (
-    <div className="hero min-h-screen bg-base-200">
-      <div className="text-center hero-content">
-        <div className="max-w-md">
-          <h1 className="mb-5 text-5xl font-bold">
-            رياكت بالعربي react<br />
-            رياكت بالعربي react<br />
-            رياكت بالعربي react<br />
-            رياكت بالعربي react<br />
-            رياكت بالعربي react<br />
-            رياكت بالعربي react<br />
-            رياكت بالعربي react<br />
-            رياكت بالعربي react<br />
-            رياكت بالعربي react<br />
-            رياكت بالعربي react<br />
-            رياكت بالعربي react<br />
-            رياكت بالعربي react<br />
-            رياكت بالعربي react<br />
-            رياكت بالعربي react<br />
-            رياكت بالعربي react<br />
-            رياكت بالعربي react<br />
-            رياكت بالعربي react<br />
-            رياكت بالعربي react<br />
-            رياكت بالعربي react<br />
-            رياكت بالعربي react<br />
-            رياكت بالعربي react<br />
-
-          </h1>
-          <p className="mb-5">
-            تعلم رياكت باللغة العربية
-          </p>
-          <button className="btn btn-primary">تعلم الآن</button>
-        </div>
-      </div>
-    </div>
-  );
-}
+import Homepage from './components/home/Homepage';
 
 function App() {
   return (
     <div dir='rtl'>
       <Layout>
-        <Sample />
+        <Homepage />
       </Layout>
     </div>
   );
--- web-app/src/components/shared/Layout.jsx
+++ web-app/src/components/shared/Layout.jsx
@@ -59,7 +59,7 @@ function WebsiteName(props) {
 	);
 }
 
-function Navbar() {
+function Navbar(props) {
 
 	function toggleDrawer() {
 		const drawer = document.getElementById(drawerId);
@@ -76,17 +76,17 @@ function Navbar() {
 	}
 
 	return (
-		<div className="navbar z-40 sticky top-0 bg-base-100 bg-opacity-90 backdrop-blur border-b-2 border-b-primary-content py-8">
+		<div className={"navbar z-40 sticky top-0 bg-opacity-90 backdrop-blur py-8 bg-base text-primary-content " + (props.isSticky && "bg-base-100 text-primary-focus border-b-2 border-b-primary-content")}>
 			<div className="flex-none tooltip tooltip-bottom" data-tip="القائمة">
 				<div>
-					<button className="btn btn-square btn-ghost lg:hidden" onClick={toggleDrawer}>
+					<button className={"btn btn-square btn-ghost " + (props.isSticky ? "lg:hidden" : "")} onClick={toggleDrawer}>
 						<Icons.Menu />
 					</button>
 				</div>
 			</div>
 			<div className="flex-1">
 				<div className="tooltip tooltip-bottom" data-tip="الرئيسية">
-					<WebsiteName className="lg:hidden " />
+					<WebsiteName className={"lg:hidden " + (props.isSticky || "hidden")} />
 				</div>
 			</div>
 			<div className="flex-none">
@@ -114,11 +114,19 @@ function Navbar() {
 
 
 function Drawer(props) {
+	function handleScroll(event) {
+		if (event.target.scrollTop <= 10) {
+			props.setIsSticky(false);
+		}
+		else {
+			props.setIsSticky(true);
+		}
+	};
 	return (
 		<div>
-			<div className="drawer drawer-mobile">
+			<div className={"drawer " + (props.isSticky && " drawer-mobile") }>
 				<input id={drawerId} type="checkbox" className="drawer-toggle" />
-				<div className="drawer-content flex flex-col primary-scrollbar">
+				<div className="drawer-content flex flex-col primary-scrollbar" onScroll={handleScroll}>
 					{props.children}
 				</div>
 				<div className="drawer-side primary-scrollbar">
@@ -128,66 +136,8 @@ function Drawer(props) {
 							<WebsiteName />
 						</div>
 						<div className="p-2 ps-4">
-							<div>Hello</div>
-							<div>Hello</div>
-							<div>Hello</div>
-							<div>Hello</div>
-							<div>Hello</div>
-							<div>Hello</div>
-							<div>Hello</div>
-							<div>Hello</div>
-							<div>Hello</div>
-							<div>Hello</div>
-							<div>Hello</div>
-							<div>Hello</div>
-							<div>Hello</div>
-							<div>Hello</div>
-							<div>Hello</div>
-							<div>Hello</div>
-							<div>Hello</div>
-							<div>Hello</div>
-							<div>Hello</div>
-							<div>Hello</div>
-							<div>Hello</div>
-							<div>Hello</div>
-							<div>Hello</div>
-							<div>Hello</div>
-							<div>Hello</div>
-							<div>Hello</div>
-							<div>Hello</div>
-							<div>Hello</div>
-							<div>Hello</div>
-							<div>Hello</div>
-							<div>Hello</div>
-							<div>Hello</div>
-							<div>Hello</div>
-							<div>Hello</div>
-							<div>Hello</div>
-							<div>Hello</div>
-							<div>Hello</div>
-							<div>Hello</div>
-							<div>Hello</div>
-							<div>Hello</div>
-							<div>Hello</div>
-							<div>Hello</div>
-							<div>Hello</div>
-							<div>Hello</div>
-							<div>Hello</div>
-							<div>Hello</div>
-							<div>Hello</div>
-							<div>Hello</div>
-							<div>Hello</div>
-							<div>Hello</div>
-							<div>Hello</div>
-							<div>Hello</div>
-							<div>Hello</div>
-							<div>Hello</div>
-							<div>Hello</div>
-							<div>Hello</div>
-							<div>Hello</div>
-							<div>Hello</div>
-							<div>Hello</div>
-
+							{/* loop 100 times */}
+							{Array.from(Array(100).keys()).map((i) => (<div key={i}>Hello</div>))}
 						</div>
 					</div>
 				</div>
@@ -197,6 +147,8 @@ function Drawer(props) {
 }
 
 export default function Layout(props) {
+	const [isSticky, setIsSticky] = useState(false);
+
 	const [firstRender, setFirstRender] = useState(true);
 	useEffect(() => {
 		if (firstRender) {
@@ -206,8 +158,8 @@ export default function Layout(props) {
 	}, [firstRender])
 	return (
 		<div>
-			<Drawer>
-				<Navbar />
+			<Drawer setIsSticky={setIsSticky} isSticky={isSticky}>
+				<Navbar isSticky={isSticky} />
 				<div>
 					{props.children}
 				</div>
