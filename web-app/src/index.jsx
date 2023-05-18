ed1109fb92b8c83f13b34197eca55c971e1ea384
Saad5400 <sdbtwa@gmail.com>
2023-05-18 21:16:39 +0300

    using react router

--- web-app/src/index.jsx
+++ web-app/src/index.jsx
@@ -3,12 +3,29 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Homepage from './pages/Homepage';
+import AboutReactIndex from './pages/about-react';
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <BrowserRouter>
+      <App>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="about-react" element={<AboutReactIndex />} />
+        </Routes>
+      </App>
+    </BrowserRouter>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
