24f1bad16b63968d7f3aa3e5ad2cb8ab42757cfd
Saad5400 <sdbtwa@gmail.com>
2023-05-18 21:24:55 +0300

    fixed basename

--- web-app/src/index.jsx
+++ web-app/src/index.jsx
@@ -17,7 +17,7 @@ import {
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='Learn-React-In-Arabic'>
       <App>
         <Routes>
           <Route path="/" element={<Homepage />} />
