36213997ac85e3ba4636f61df7949b6e44feeea7
Saad5400 <sdbtwa@gmail.com>
2023-05-20 08:28:53 +0300

    just testing

--- web-app/src/pages/about-react/index.jsx
+++ web-app/src/pages/about-react/index.jsx
@@ -1,10 +1,12 @@
+import Container from "../../components/shared/Container";
+
 export default function Index() {
     return (
-        <div>
-            Hello World
-            <div className="h-screen">
-                
-            </div>
-        </div>
+        <Container>
+            <article className="prose">
+                <h2>About React</h2>
+                <p>React is a JavaScript library for building user interfaces. It is maintained by Facebook and a community of individual developers and companies.</p>
+            </article>
+        </Container>
     );
 }
